f1dc5aa6bf1c8dadf6f99f088adb0c06be50002f
Martien Dermawan Tanama <martiendt@gmail.com>
2023-02-26 15:42:31 +0700

    update content

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4999
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello to World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example to app listening on port ${port}`)
 })
